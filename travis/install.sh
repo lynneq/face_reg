890f13591cd4684d3b7501a9ee8067607294f8a9
lynneq <lqu@thoughtworks.com>
2013-07-11 01:34:43 -0700

    Try install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -11,10 +11,14 @@ wget http://dl.google.com/android/android-sdk_r21.0.1-linux.tgz
 tar -xzf android-sdk_r21.0.1-linux.tgz
 export ANDROID_HOME=$PROJECT_ROOT/android-sdk-linux
 export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
+echo $PATH
 android update sdk --no-ui --force --filter platform-tools,extra-android-support,android-17,sysimg-17,extra-google-google_play_services
+
+echo "git submodule update started ..............."
 git submodule foreach git pull origin master
-cp vendor/gms-mvn/gms-mvn-install.sh android-sdk-linux/extras/google/google_play_services/libproject/google-play-services_lib/ 
-(cd android-sdk-linux/extras/google/google_play_services/libproject/google-play-services_lib/ && ./gms-mvn-install.sh 7)
+echo "git submodule update done ..............."
+#cp vendor/gms-mvn/gms-mvn-install.sh android-sdk-linux/extras/google/google_play_services/libproject/google-play-services_lib/ 
+#(cd android-sdk-linux/extras/google/google_play_services/libproject/google-play-services_lib/ && ./gms-mvn-install.sh 7)
 echo no | android create avd -n emulator -t android-17 --skin WVGA800 --force --abi armeabi-v7a
 gem install calabash-android
 echo "end....."
